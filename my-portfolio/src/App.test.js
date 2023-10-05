5da3b8846a8fac1771ee8dfc6b8e5b5747b8f7af
ashutoshkumar1020 <115533717+ashutoshkumar1020@users.noreply.github.com>
2023-10-05 12:29:07 +0530

    done

--- my-portfolio/src/App.test.js
+++ my-portfolio/src/App.test.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders App component', () => {
-  const { getByText } = render(<App />);
-  
-  const homeElement = getByText('Home');
-  const aboutElement = getByText('About');
-  const skillsElement = getByText('Skills');
-  const projectsElement = getByText('Projects');
-  const contactElement = getByText('Contact');
-
-  expect(homeElement).toBeInTheDocument();
-  expect(aboutElement).toBeInTheDocument();
-  expect(skillsElement).toBeInTheDocument();
-  expect(projectsElement).toBeInTheDocument();
-  expect(contactElement).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
